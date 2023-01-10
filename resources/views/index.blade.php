eafdf6b81f27577c0ad222831a7b14c583cd56ee
Maira2789 <mayte270597@gmail.com>
2023-01-09 19:04:27 -0600

    add some characters and designs for frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,20 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel= "icon" href="{{asset('img/moviez.jpg')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <title>Moviez</title>
- </head>
- <body>
- <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <script>src="https://code.jquery.com/jquery-3.6.3.min.js"</script>
+   <title>Moviez</title>
+   <style>
+        body{
+         background: url("https://cdn.pixabay.com/photo/2017/09/12/11/56/universe-2742113_960_720.jpg"); 
+         background-repeat: no-repeat;
+         background-size: 100vw 100vh ;
+         z-index: -3;
+         background-attachment:fixed:
+        }
+        @font-face {
+         font-family:'Positive System' ;
+         src: url('fonts/Positive System.otf');
+        }
+        .navbar-brand{
+         font-family:'Positive System' ;
+         font-size:35px;
+        }
+       
+   </style>
+</head>
+
+ <body> 
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+   
+    <a class="navbar-brand" href="#">
+      <img src="{{asset('img/camara.jpg')}}" alt="movies" width="60px">
+    MOVIEZ</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#" >Inicio</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
@@ -41,8 +65,13 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <div>
+         <<button type="button" class="btn btn-outline-light">Log in</button>
+      </div>
+      </div>
     </div>
   </div>
 </nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
  </body>
  </html>
